958872dbc83ab1641461397bc946670b1937d8c7
MarceloSCarvalho <164251829+MarceloSCarvalho@users.noreply.github.com>
2025-11-12 18:12:40 +0000

    adicionar página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1> Nossas ações de hoje moldam o futuro — mas, se pudéssemos voltar ao passado, será que teríamos coragem de mudar o que nos trouxe até aqui? </h1>
+}
+
+export default Home;
